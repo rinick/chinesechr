1953b52673a7299a42a149ef9114c8b802e55556
rinick <rinick@gmail.com>
2022-10-09 19:42:25 -0700

    fix base url

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    base: './',
     assetsDir: './',
     outDir: 'docs',
     rollupOptions: {
