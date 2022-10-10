97a2cf5ed8520f6cad311f8b3df503bc1d0452ce
rinick <rinick@gmail.com>
2022-10-09 18:30:23 -0700

    gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  build: {
+    base: './',
+    assetsDir: './',
+    outDir: 'docs',
+    rollupOptions: {
+      // https://rollupjs.org/guide/en/#big-list-of-options
+    },
+  },
+});
